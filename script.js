6a1c3eacce66c4b5c5e950059e24305b2ac5343b
Łukasz Jaroszewski <lukejar10@gmail.com>
2024-10-07 22:52:21 +0200

    Home and services sections' improvements
    Added new logo img and changed logos. Modified and improved Home sections layout. Added another device to CSS (min. 1600px). Adjusted position of a hint in Our Services section on smaller screens.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const observerOptions = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.9
+        threshold: 0.7
     };
 
     const observer = new IntersectionObserver((entries) => {
